76779f4347c54818901776c950c6eabf2510b781
Marco Tedone <marco@techwings.io>
2020-08-02 22:43:14 +0100

    Add checks for double bidding and same seller bidding

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,25 +10,40 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
+  // Auction status validation
   if (auction.status !== 'OPEN') {
     throw new createError.Forbidden('You cannot bid on closed options');
   }
 
+  // The highest bid must be greater than the current bid
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(
       `Your bid must be higher than ${auction.highestBid.amount}`
     );
   }
 
+  // Sellers can't bid on their own auctions
+  if (auction.seller === email) {
+    throw new createError.Forbidden(`You can't bid on your own auctions!`);
+  }
+
+  // Highest bidders can't bid while they have the highest bid
+  if (auction.highestBid.bidder === email) {
+    throw new createError.Forbidden('You are already the highest bidder!');
+  }
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression:
+      'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   };
